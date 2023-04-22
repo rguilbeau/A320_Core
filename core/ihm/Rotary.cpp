8dd6e148fd0beec180d0637e6e27319e60e863df
Romain Guilbeau <guilbeau.r@gmail.com>
2023-04-22 12:03:25 +0200

    correction lecture du rotary encoder, pour une raison inconnu je doit passer en half detection, et donc inhiber un event sur 2

--- core/ihm/Rotary.cpp
+++ core/ihm/Rotary.cpp
@@ -1,20 +1,18 @@
 #include "Rotary.h"
 
-const unsigned char Rotary::_tableState[7][4] = {
-  // R_START
-  {R_START,    R_CW_BEGIN,  R_CCW_BEGIN, R_START},
-  // R_CW_FINAL
-  {R_CW_NEXT,  R_START,     R_CW_FINAL,  R_START | R_DIR_CW},
-  // R_CW_BEGIN
-  {R_CW_NEXT,  R_CW_BEGIN,  R_START,     R_START},
-  // R_CW_NEXT
-  {R_CW_NEXT,  R_CW_BEGIN,  R_CW_FINAL,  R_START},
+const unsigned char Rotary::_tableState[6][4] = {
+  // R_START (00)
+  {R_START_M,            R_CW_BEGIN,     R_CCW_BEGIN,  R_START},
   // R_CCW_BEGIN
-  {R_CCW_NEXT, R_START,     R_CCW_BEGIN, R_START},
-  // R_CCW_FINAL
-  {R_CCW_NEXT, R_CCW_FINAL, R_START,     R_START | R_DIR_CCW},
-  // R_CCW_NEXT
-  {R_CCW_NEXT, R_CCW_FINAL, R_CCW_BEGIN, R_START},
+  {R_START_M | DIR_CCW, R_START,        R_CCW_BEGIN,  R_START},
+  // R_CW_BEGIN
+  {R_START_M | DIR_CW,  R_CW_BEGIN,     R_START,      R_START},
+  // R_START_M (11)
+  {R_START_M,            R_CCW_BEGIN_M,  R_CW_BEGIN_M, R_START},
+  // R_CW_BEGIN_M
+  {R_START_M,            R_START_M,      R_CW_BEGIN_M, R_START | DIR_CW},
+  // R_CCW_BEGIN_M
+  {R_START_M,            R_CCW_BEGIN_M,  R_START_M,    R_START | DIR_CCW},
 };
 
 Rotary::Rotary(InputInterface *inputA, InputInterface *inputB)
@@ -23,6 +21,7 @@ Rotary::Rotary(InputInterface *inputA, InputInterface *inputB)
     _inputB = inputB;
     _event = nullptr;
     _lastState = R_START;
+    _countEvent = 0;
 }
 
 Rotary::Rotary(InputInterface *inputA, InputInterface *inputB, RotaryEvent *event)
@@ -31,6 +30,7 @@ Rotary::Rotary(InputInterface *inputA, InputInterface *inputB, RotaryEvent *even
     _inputB = inputB;
     _event = event;
     _lastState = R_START;
+    _countEvent = 0;
 }
 
 bool Rotary::loop()
@@ -38,6 +38,7 @@ bool Rotary::loop()
     // Grab state of input pins.
     unsigned char valueA = _inputA->read() ? 0x1 : 0x0;
     unsigned char valueB = _inputB->read() ? 0x1 : 0x0;
+
     unsigned char rotaryPinsValue = (valueA << 1) | valueB;
     // Determine new state from the pins and state table.
     _lastState = _tableState[_lastState & 0xf][rotaryPinsValue];
@@ -45,12 +46,22 @@ bool Rotary::loop()
     unsigned char currentState = _lastState & 0x30;
 
     if(_event != nullptr) {
-        if(currentState == R_DIR_CW) {
-            _event->onMove(RotaryEventDirection::INCR);
-            return true;
-        } else if(currentState == R_DIR_CCW) {
-            _event->onMove(RotaryEventDirection::DECR);
-            return true;
+        if(currentState == DIR_CW) {
+            _countEvent++;
+
+            if(_countEvent > 1) {
+                _countEvent = 0;
+                _event->onMove(RotaryEventDirection::INCR);
+                return true;
+            }
+        } else if(currentState == DIR_CCW) {
+            _countEvent++;
+            
+            if(_countEvent > 1) {
+                _countEvent = 0;
+                _event->onMove(RotaryEventDirection::DECR);
+                return true;
+            }
         }
     }
 
--- core/ihm/Rotary.h
+++ core/ihm/Rotary.h
@@ -6,7 +6,7 @@
 #include "core/ihm/IhmInterface.h"
 #include "core/event/RotaryEvent.h"
 
-#define R_CW_FINAL 0x1
+/*#define R_CW_FINAL 0x1
 #define R_CW_BEGIN 0x2
 #define R_CW_NEXT 0x3
 #define R_CCW_BEGIN 0x4
@@ -14,7 +14,23 @@
 #define R_CCW_NEXT 0x6
 #define R_START 0x0
 #define R_DIR_CW 0x10
-#define R_DIR_CCW 0x20
+#define R_DIR_CCW 0x20*/
+
+// Values returned by 'process'
+// No complete step yet.
+#define DIR_NONE 0x0
+// Clockwise step.
+#define DIR_CW 0x10
+// Counter-clockwise step.
+#define DIR_CCW 0x20
+
+#define R_START 0x0
+
+#define R_CCW_BEGIN 0x1
+#define R_CW_BEGIN 0x2
+#define R_START_M 0x3
+#define R_CW_BEGIN_M 0x4
+#define R_CCW_BEGIN_M 0x5
 
 class Rotary : public IhmInterface {
 
@@ -28,7 +44,8 @@ class Rotary : public IhmInterface {
         InputInterface *_inputB;
         RotaryEvent *_event;
         unsigned char _lastState;
-        static const unsigned char _tableState[7][4]; 
+        short _countEvent;
+        static const unsigned char _tableState[6][4]; 
 };
 
 
