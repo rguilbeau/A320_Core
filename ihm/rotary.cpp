f3ba504325f0df040cc06af29273e12f629d282c
Romain Guilbeau <guilbeau.r@gmail.com>
2025-03-01 15:41:56 +0100

    Change rotary envoder library

--- ihm/rotary.cpp
+++ ihm/rotary.cpp
@@ -1,73 +1,38 @@
 #include "rotary.hpp"
 
-const uint8_t Rotary::m_tableState[6][4] = {
-  // R_START (00)
-  {R_START_M,            R_CW_BEGIN,     R_CCW_BEGIN,  R_START},
-  // R_CCW_BEGIN
-  {R_START_M | DIR_CCW, R_START,        R_CCW_BEGIN,  R_START},
-  // R_CW_BEGIN
-  {R_START_M | DIR_CW,  R_CW_BEGIN,     R_START,      R_START},
-  // R_START_M (11)
-  {R_START_M,            R_CCW_BEGIN_M,  R_CW_BEGIN_M, R_START},
-  // R_CW_BEGIN_M
-  {R_START_M,            R_START_M,      R_CW_BEGIN_M, R_START | DIR_CW},
-  // R_CCW_BEGIN_M
-  {R_START_M,            R_CCW_BEGIN_M,  R_START_M,    R_START | DIR_CCW},
-};
-
-Rotary::Rotary(InputInterface *pInputA, InputInterface *pInputB)
+Rotary::Rotary(InputInterface *pInputA, InputInterface *pInputB): 
+    m_rotaryEncoder{pInputA, pInputB, RotaryEncoder::LatchMode::FOUR0}
 {
-    m_pInputA = pInputA;
-    m_pInputB = pInputB;
     m_pEvent = nullptr;
-    m_bLastState = R_START;
-    m_nCountEvent = 0;
 }
 
-Rotary::Rotary(InputInterface *pInputA, InputInterface *pInputB, RotaryEvent *pEvent)
+Rotary::Rotary(InputInterface *pInputA, InputInterface *pInputB, RotaryEvent *pEvent):
+    m_rotaryEncoder{pInputA, pInputB, RotaryEncoder::LatchMode::FOUR0}
 {
-    m_pInputA = pInputA;
-    m_pInputB = pInputB;
     m_pEvent = pEvent;
-    m_bLastState = R_START;
-    m_nCountEvent = 0;
 }
 
 bool Rotary::loop()
 {   
-    // Grab state of input pins.
-    uint8_t nValueA = m_pInputA->read() ? 0x1 : 0x0;
-    uint8_t nValueB = m_pInputB->read() ? 0x1 : 0x0;
+    m_rotaryEncoder.tick();
+    int nNewPos = m_rotaryEncoder.getPosition();
 
-    uint8_t nRotaryPinsValue = (nValueA << 1) | nValueB;
-    // Determine new state from the pins and state table.
-    m_bLastState = m_tableState[m_bLastState & 0xf][nRotaryPinsValue];
-    // Return emit bits, ie the generated event.
-    uint8_t nCurrentState = m_bLastState & 0x30;
-
-    if(m_pEvent != nullptr) 
+    if (m_currentPos != nNewPos) 
     {
-        if(nCurrentState == DIR_CW) 
-        {
-            m_nCountEvent++;
+        RotaryEncoder::Direction direction = m_rotaryEncoder.getDirection();
 
-            if(m_nCountEvent > 1) 
-            {
-                m_nCountEvent = 0;
-                m_pEvent->onMove(RotaryEvent::e_direction::INCR);
-                return true;
-            }
-        } else if(nCurrentState == DIR_CCW) 
+        if(direction == RotaryEncoder::Direction::COUNTERCLOCKWISE)
         {
-            m_nCountEvent++;
-            
-            if(m_nCountEvent > 1) 
-            {
-                m_nCountEvent = 0;
-                m_pEvent->onMove(RotaryEvent::e_direction::DECR);
-                return true;
-            }
+            m_pEvent->onMove(RotaryEvent::e_direction::INCR);
+            return true;
         }
+        else if(direction == RotaryEncoder::Direction::CLOCKWISE)
+        {
+            m_pEvent->onMove(RotaryEvent::e_direction::DECR);
+            return true;
+        }
+
+        m_currentPos = nNewPos;
     }
 
     return false;
--- ihm/rotary.hpp
+++ ihm/rotary.hpp
@@ -5,22 +5,7 @@
 #include "input/input_interface.hpp"
 #include "ihm/ihm_interface.hpp"
 #include "event/rotary_event.hpp"
-
-// Values returned by 'process'
-// No complete step yet.
-#define DIR_NONE 0x0
-// Clockwise step.
-#define DIR_CW 0x10
-// Counter-clockwise step.
-#define DIR_CCW 0x20
-
-#define R_START 0x0
-
-#define R_CCW_BEGIN 0x1
-#define R_CW_BEGIN 0x2
-#define R_START_M 0x3
-#define R_CW_BEGIN_M 0x4
-#define R_CCW_BEGIN_M 0x5
+#include "lib/RotaryEncoder.h"
 
 class Rotary : public IhmInterface 
 {
@@ -33,12 +18,14 @@ public:
     bool loop() override;
 
 private:
+    RotaryEncoder m_rotaryEncoder;
     InputInterface *m_pInputA;
     InputInterface *m_pInputB;
     RotaryEvent *m_pEvent;
     uint8_t m_bLastState;
     uint16_t m_nCountEvent;
-    static const uint8_t m_tableState[6][4]; 
+
+    int m_currentPos = 0;
 };
 
 
